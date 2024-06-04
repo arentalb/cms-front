8e1a9b3047832f71f246f49aa994d20d974f63b1
aren talb <aren.talb00@gmail.com>
2024-06-04 14:53:11 +0300

    Basic setup for local translation

---
+++ src/i18n/index.js
@@ -0,0 +1,29 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import enTranslation from "./locales/en/translation.json";
+import frTranslation from "./locales/fr/translation.json";
+import krTranslation from "./locales/kr/translation.json";
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    resources: {
+      en: {
+        translation: enTranslation,
+      },
+      fr: {
+        translation: frTranslation,
+      },
+      kr: {
+        translation: krTranslation,
+      },
+    },
+    lng: "en", // default language
+    fallbackLng: "en",
+
+    interpolation: {
+      escapeValue: false, // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
+    },
+  });
+
+export default i18n;
