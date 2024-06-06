e95ed3cacd70649519a849068cae3bdebe64956e
aren talb <aren.talb00@gmail.com>
2024-06-06 05:42:50 +0300

    Fix the design of the model

--- src/components/common/EditableText.jsx
+++ src/components/common/EditableText.jsx
@@ -71,7 +71,7 @@ export function EditableText({ translationKey }) {
             onClick={handleSave}
             className="bg-green-600 text-white px-4 py-2 rounded-md text-lg"
           >
-            Save
+            {i18n.language === "kr" ? "هەڵگرتن" : "Save"}
           </button>
         </div>
       </Modal>
--- src/components/common/Modal.jsx
+++ src/components/common/Modal.jsx
@@ -1,14 +1,20 @@
+import { useTranslation } from "react-i18next";
+
 export function Modal({ isOpen, onClose, children }) {
+  const { i18n } = useTranslation("header");
+
   if (!isOpen) return null;
 
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50 ">
       <div className="fixed inset-0 bg-black opacity-50"></div>
-      <div className="bg-white p-6 rounded shadow-lg z-10 w-2/3">
+      <div className="bg-white p-6 rounded shadow-lg z-10 w-full mx-4 sm:mx-0 sm:w-2/3 sm:w-1/3 text-sm">
         <div className="flex justify-between items-center mb-4">
-          <h2 className="text-xl font-semibold">Edit Text</h2>
-          <button onClick={onClose} className="text-red-600">
-            &times;
+          <h2 className="text-xl font-semibold text-black">
+            {i18n.language === "kr" ? "دەستکاری دەق" : "Edit Text"}
+          </h2>
+          <button onClick={onClose} className="text-red-600 ">
+            <img src="/x.svg" alt="" />
           </button>
         </div>
         {children}
